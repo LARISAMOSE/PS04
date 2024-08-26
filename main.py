65282f5fd2dd0ac0b2ca3133f39d8b6c576a9c41
LARISAMOSE <mosevica@gmail.com>
2024-08-26 14:29:57 +0200

    add PS04.3

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import time
 browser = webdriver.Chrome()
-browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
-assert "Википедия" in browser.title
-search_box = browser.find_element(By.ID,"searchInput" )
-search_box.send_keys("Солнечная система")
-time.sleep(15)
-search_box.send_keys(Keys.RETURN)
-time.sleep(15)
-a = browser.find_element(By.LINK_TEXT, "Солнечная система")
-a.click()
-time.sleep(10)
+browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
+paragraphs = browser.find_elements(By.TAG_NAME, "p")
+
+for paragraph in paragraphs:
+    print(paragraph.text)
+    input("Нажмите enter для продолжения")
+
+
+
+
