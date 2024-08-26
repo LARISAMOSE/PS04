bac4cbdbfbe8af856af3810df15d6470f143317b
LARISAMOSE <mosevica@gmail.com>
2024-08-26 13:55:00 +0200

    add PS04.2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 import time
 browser = webdriver.Chrome()
-browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
-browser.save_screenshot("dom.png")
+browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+assert "Википедия" in browser.title
+search_box = browser.find_element(By.ID,"searchInput" )
+search_box.send_keys("Солнечная система")
+time.sleep(15)
+search_box.send_keys(Keys.RETURN)
+time.sleep(15)
+a = browser.find_element(By.LINK_TEXT, "Солнечная система")
+a.click()
 time.sleep(10)
-browser.get("https://ru.wikipedia.org/wiki/Selenium")
-browser.save_screenshot("selenium.png")
-time.sleep(5)
-browser.refresh()
